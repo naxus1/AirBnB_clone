d47e3eea5f7042294611505f13ae9a4f405def79
naxus1 <naxus1@gmail.com>
2019-07-04 02:18:53 +0000

    Pep8

--- models/user.py
+++ models/user.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
-
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
     """Create users"""
 
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
-
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
