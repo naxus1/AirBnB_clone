845eefa7f312c69dd3d5fa58ac5b993262f75554
naxus1 <naxus1@gmail.com>
2019-07-04 02:36:39 +0000

    Create Clases

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""model Amenity"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class to create the Amenity""
+
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class City"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """class City to crete a City object"""
+
+    state_id = ""
+    name = ""
