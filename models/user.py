02d9373a30f98d0ed8c73d1ce22d5517118efad7
naxus1 <naxus1@gmail.com>
2019-07-04 02:23:14 +0000

    Add coments

--- models/user.py
+++ models/user.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
+"""User module"""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """Create users"""
+    """Create users
+    Attributes:
+        email (str): email.
+        password (str): password.
+        first_name (str): first name.
+        last_name (str): last name.
+    """
 
     email = ""
     password = ""
