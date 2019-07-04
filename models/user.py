29fc13bf2640c78b1fe390b8895e2bc4e83d7be5
naxus1 <naxus1@gmail.com>
2019-07-04 02:16:45 +0000

    Pep8

--- models/user.py
+++ models/user.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
-import uuid
-from datetime import datetime, date, time
-from models import *
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """Create users"""
 
     def __init__(self, *args, **kwargs):
         super().__init__(self, *args, **kwargs)
